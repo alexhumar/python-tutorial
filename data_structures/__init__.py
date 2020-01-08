58efa445e1a6c9fbef2b8ecb21015a3873b59d79
Alex Humar <alexhumar@yahoo.com>
2020-01-07 22:41:25 -0300

    Organizacion de los modules (archivos) en packages (directorios)

---
+++ data_structures/__init__.py
@@ -0,0 +1,2 @@
+# This file tells the interpreter that this folder is a python package
+# Packages are the way to group modules in python
--- modules.py
+++ modules.py
@@ -2,10 +2,11 @@
 # should contain related functions and classes
 # Check file converters.py
 
-# With this, we import module converters, and now converters is an object
-import converters
-# We also can import specific functions of a module
-from converters import lbs_to_kg
+# With this, we import module converters from functions package, and now converters is an object
+from functions import converters
+# We also can import specific functions of a module. Since converters is in package functions, we must prefix the
+# package name too.
+from functions.converters import lbs_to_kg
 
 print(converters.kg_to_lbs(75))
 print(converters.lbs_to_kg(160))
