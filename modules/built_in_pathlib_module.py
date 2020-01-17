fc5a25f84577d1962f9d785afb6db97f841d527c
Alex Humar <alexhumar25@gmail.com>
2020-01-16 23:12:30 -0300

    Comienzo de pypi

--- modules/built_in_pathlib_module.py
+++ modules/built_in_pathlib_module.py
@@ -5,3 +5,13 @@ from pathlib import Path
 path = Path('.')
 print(path.exists())
 print(path.absolute())
+
+# Files and directories
+print(path.glob('*'))
+# Only files
+print(path.glob('*.*'))
+# The methods above return a generator object. I NEED TO INVESTIGATE THE SUBJECT.
+
+# Prints all the .py files in the current directory
+for file in path.glob('*.py'):
+    print(file)
---
+++ modules/pypi.py
@@ -0,0 +1,6 @@
+# pypi stands for Python Project index and is a "repository" of Python packaged made by the community that we can
+# install and use.
+# Any package can be installed by executing the following command:
+# pip install [package_name]. That command doesn't work. I had to use: python3.8 -m pip install openpyxl
+# in a terminal, within the python project root directory
+# Packages installed by pip will be places in External Libraries -> Python 3.X -> site-packages
